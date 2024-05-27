d24afb78959c6f699f15fb95fe867e41217c0493
tochouseito <k023g0060@g.neec.ac.jp>
2024-05-27 18:25:14 +0900

    UNど完成

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Principal;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManagerScript : MonoBehaviour
 {
@@ -10,23 +11,48 @@ public class GameManagerScript : MonoBehaviour
     public GameObject goalsPrefab;
     public GameObject clearText;
     public GameObject particlePrefab;
+    public GameObject wallPrefab;
+    public GameObject player = null;
+    public Quaternion targetRotation = Quaternion.identity;
+    public Stack<GameAction> actionStack = new Stack<GameAction>();
+    int stock = 0;
+    //public Stack<GameAction> boxStack = new Stack<GameAction>();
+    public enum ActionType
+    {
+        MovePlayer,
+        MoveBox
+    }
+    public class GameAction
+    {
+        public ActionType actionType;
+        public Vector2Int oldPosition;
+        public Vector2Int newPosition;
+        public string tags;
+        public GameObject target;  // プレイヤーまたは箱
+    }
     int[,] map;
     GameObject[,] field;
     GameObject obj;
-
+    public float rotationSpeed = 180.0f;
+    public Vector3 before ;
     string debugText = "";
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(1280, 720, false);
+        Screen.SetResolution(1920, 1080, false);
 
         map = new int[,] {
-        {0,3,2,0,0,0,0},
-        {0,0,1,3,2 ,0,0},
-        {3,2,0,0,0 ,0,0},
-        {3,2,0,0,0 ,0,0},
-        {3,2,0,0,0 ,0,0}
+        {4,4,4,4,4,4,4,4,4,4},
+        {4,3,0,0,0,0,0,0,0,4},
+        {4,4,4,4,0,0,0,0,0,4},
+        {4,0,0,0,0,0,0,0,0,4},
+        {4,0,2,3,2,0,0,0,0,4},
+        {4,0,0,0,1,0,0,0,0,4},
+        {4,0,0,0,0,0,0,0,0,4},
+        {4,0,0,0,0,0,0,0,0,4},
+        {4,0,0,0,0,0,0,0,0,4},
+        {4,4,4,4,4,4,4,4,4,4}
         };
         field = new GameObject[
             map.GetLength(0),
@@ -39,7 +65,7 @@ public class GameManagerScript : MonoBehaviour
             {
                 if (map[y,x] == 1)
                 {
-                    GameObject player = Instantiate(playerPrefab,
+                   player = Instantiate(playerPrefab,
                         new Vector3(x - (map.GetLength(1) / 2), (map.GetLength(0)/2)-y, 0),
                         Quaternion.identity);
                     field[y, x] = player;
@@ -54,9 +80,16 @@ public class GameManagerScript : MonoBehaviour
                 if (map[y, x] == 3)
                 {
                     GameObject Goals = Instantiate(goalsPrefab,
-                       new Vector3(x - (map.GetLength(1) / 2), map.GetLength(0)/2 - y, 0.01f),
+                       new Vector3(x - (map.GetLength(1) / 2), map.GetLength(0)/2 - y, -0.4f),
                        Quaternion.identity);
                     //field[y, x] = Goals;
+                }
+                if (map[y,x]==4)
+                {
+                    GameObject Walls=Instantiate(wallPrefab,
+                        new Vector3(x-(map.GetLength(1)/2),map.GetLength(0)/2-y,0),
+                        Quaternion.identity);
+                    field[y, x] = Walls;
                 }
                 //GameObject ClearText = Instantiate(clearText,
                    //    new Vector3(x, map.GetLength(0) - y, 0),
@@ -73,10 +106,13 @@ public class GameManagerScript : MonoBehaviour
     void Update()
     {
    
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow)||Input.GetKeyDown(KeyCode.D))
         {
+            stock = actionStack.Count;
+            targetRotation = Quaternion.Euler(0, 0, 0);
+            
             Vector2Int playerIndex =GetPlayerIndex();
-            MoveNumber(tag, playerIndex, playerIndex+new Vector2Int(1,0));
+            MoveNumber("Player", playerIndex, playerIndex+new Vector2Int(1,0));
             for (int i = 0; i < 8; ++i)
             {
                 GameObject particle = Instantiate(particlePrefab,
@@ -85,10 +121,12 @@ public class GameManagerScript : MonoBehaviour
             }
 
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
+            stock = actionStack.Count;
+            targetRotation = Quaternion.Euler(0, 0, 180);
             Vector2Int playerIndex = GetPlayerIndex();
-            MoveNumber(tag, playerIndex, playerIndex+new Vector2Int(-1,0));
+            MoveNumber("Player", playerIndex, playerIndex+new Vector2Int(-1,0));
             for (int i = 0; i < 8; ++i)
             {
                 GameObject particle = Instantiate(particlePrefab,
@@ -96,10 +134,12 @@ public class GameManagerScript : MonoBehaviour
                        Quaternion.identity);
             }
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
+            stock = actionStack.Count;
+            targetRotation = Quaternion.Euler(0, 0, 90);
             Vector2Int playerIndex = GetPlayerIndex();
-            MoveNumber(tag, playerIndex, playerIndex + new Vector2Int(0, -1));
+            MoveNumber("Player", playerIndex, playerIndex + new Vector2Int(0, -1));
             for (int i = 0; i < 8; ++i)
             {
                 GameObject particle = Instantiate(particlePrefab,
@@ -107,10 +147,12 @@ public class GameManagerScript : MonoBehaviour
                        Quaternion.identity);
             }
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
+            stock = actionStack.Count;
+            targetRotation = Quaternion.Euler(0, 0, -90);
             Vector2Int playerIndex = GetPlayerIndex();
-            MoveNumber(tag, playerIndex, playerIndex + new Vector2Int(0, 1));
+            MoveNumber("Player", playerIndex, playerIndex + new Vector2Int(0, 1));
             for (int i = 0; i < 8; ++i)
             {
                 GameObject particle = Instantiate(particlePrefab,
@@ -118,10 +160,19 @@ public class GameManagerScript : MonoBehaviour
                        Quaternion.identity);
             }
         }
+
+        player.transform.rotation = Quaternion.Lerp(player.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
         if (IsCleard())
         {
 
             clearText.SetActive(true);
+        }
+        if(Input.GetKeyDown(KeyCode.R)) {
+            Reset();
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Undo();
         }
     }
     
@@ -145,18 +196,60 @@ public class GameManagerScript : MonoBehaviour
 
         return new Vector2Int(-1, -1);
     }
-   bool MoveNumber(string tag,Vector2Int moveFrom,Vector2Int moveTo)
+    bool MoveNumber(string tag, Vector2Int moveFrom, Vector2Int moveTo)
     {
         
-        if (moveTo.y < 0 || moveTo.y >= field.GetLength(0)) { return false;}
+        if (moveTo.y < 0 || moveTo.y >= field.GetLength(0)) { return false; }
         if (moveTo.x < 0 || moveTo.x >= field.GetLength(1)) { return false; }
-        if (field[moveTo.y,moveTo.x] != null && field[moveTo.y,moveTo.x].tag=="Box")
+        if (field[moveTo.y, moveTo.x] != null && field[moveTo.y, moveTo.x].tag == "Player")
         {
-            Vector2Int velocity=moveTo-moveFrom;
-            bool success = MoveNumber(tag, moveTo, moveTo + velocity);
+            Vector2Int velocity = moveTo - moveFrom;
+            bool success = MoveNumber("Player", moveTo, moveTo + velocity);
             if (!success) { return false; }
         }
-        
+        if (field[moveTo.y,moveTo.x] != null && field[moveTo.y,moveTo.x].tag=="Box")
+        {
+            
+            Vector2Int velocity=moveTo-moveFrom;
+            bool success = MoveNumber("Box", moveTo, moveTo + velocity);
+            if (!success) { return false; }
+        }
+        if (field[moveTo.y, moveTo.x] != null && field[moveTo.y, moveTo.x].tag == "Wall")
+        {
+            Vector2Int velocity = moveTo - moveFrom;
+            bool success = MoveNumber("Wall", moveTo, moveTo + velocity);
+            if (!success) { return false; }
+            if (actionStack.Count == stock)
+            {
+                actionStack.Push(new GameAction
+                {
+                    actionType = ActionType.MovePlayer,
+                    oldPosition = moveFrom,
+                    newPosition = moveTo,
+                    tags = tag
+                    //target = player
+                });
+            }
+            return false;
+            //Vector2Int velocity = moveTo - moveFrom;
+            //bool success = MoveNumber(tag, moveTo, moveTo + velocity);
+            //if (!success) { return false; }
+        }
+        if(field[moveTo.y, moveTo.x] == null)
+        {
+            if (actionStack.Count == stock)
+            {
+                actionStack.Push(new GameAction
+                {
+                    actionType = ActionType.MovePlayer,
+                    oldPosition = moveFrom,
+                    newPosition = moveTo,
+                    tags = tag
+                    //target = player
+                });
+            }
+            //return false;
+        }
         // GameObjectの座標（position)を移動させてからインデックスの入れ替え
         //field[moveFrom.y, moveFrom.x].transform.position =
         //new Vector3(moveTo.x, field.GetLength(0) - moveTo.y, 0);
@@ -168,6 +261,44 @@ public class GameManagerScript : MonoBehaviour
         field[moveFrom.y, moveFrom.x] = null;
         return true;
         
+    }
+    bool MoveUndo(string tag, Vector2Int moveFrom, Vector2Int moveTo)
+    {
+
+        if (moveTo.y < 0 || moveTo.y >= field.GetLength(0)) { return false; }
+        if (moveTo.x < 0 || moveTo.x >= field.GetLength(1)) { return false; }
+        if (field[moveTo.y, moveTo.x] != null && field[moveTo.y, moveTo.x].tag == "Player")
+        {
+            Vector2Int velocity = moveTo - moveFrom;
+            bool success = MoveUndo("Player", moveTo, moveTo + velocity);
+            if (!success) { return false; }
+        }
+        if (field[moveTo.y, moveTo.x] != null && field[moveTo.y, moveTo.x].tag == "Box")
+        {
+
+            Vector2Int velocity = moveTo - moveFrom;
+            bool success = MoveUndo("Box", moveTo, moveTo + velocity);
+            if (!success) { return false; }
+        }
+        if (field[moveTo.y, moveTo.x] != null && field[moveTo.y, moveTo.x].tag == "Wall")
+        {
+            return false;
+            //Vector2Int velocity = moveTo - moveFrom;
+            //bool success = MoveNumber(tag, moveTo, moveTo + velocity);
+            //if (!success) { return false; }
+        }
+        
+        // GameObjectの座標（position)を移動させてからインデックスの入れ替え
+        //field[moveFrom.y, moveFrom.x].transform.position =
+        //new Vector3(moveTo.x, field.GetLength(0) - moveTo.y, 0);
+        Vector3 moveToPosition = new Vector3(
+            moveTo.x - field.GetLength(1) / 2, field.GetLength(0) / 2 - moveTo.y, 0
+            );
+        field[moveFrom.y, moveFrom.x].GetComponent<Move>().MoveTo(moveToPosition);
+        field[moveTo.y, moveTo.x] = field[moveFrom.y, moveFrom.x];
+        field[moveFrom.y, moveFrom.x] = null;
+        return true;
+
     }
     bool IsCleard()
     {
@@ -201,5 +332,33 @@ public class GameManagerScript : MonoBehaviour
         Debug.Log(debugText);
         return true;
     }
+   void Reset()
+    {
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                Destroy(field[y, x]);
+            }
+        }
+        clearText.SetActive(false);
+        Start();
+    }
+    void Undo()
+    {
+        if (actionStack.Count > 0)
+        {
+            GameAction lastAction = actionStack.Pop();
+            Vector2Int oldPosition = lastAction.oldPosition;
+            Vector2Int newPosition = lastAction.newPosition;
+            
+            // プレイヤーの位置を元に戻す
+            MoveUndo(lastAction.tags, newPosition, oldPosition);
+            //lastAction.target.transform.position = new Vector3(oldPosition.x, oldPosition.y, 0);
+
+        }
         
+        
+    }
+   
 }
